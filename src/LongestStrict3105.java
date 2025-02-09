738de87f146c76a5db4014799c0ed68bb738d3f8
POI <dileeppuli3@gmail.com>
2025-02-09 23:18:13 +0530

    leetcode changes

---
+++ src/LongestStrict3105.java
@@ -0,0 +1,16 @@
+public class LongestStrict3105 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int longestMonotonicSubarray(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            count=1;
+
+
+        }
+        return 1;
+    }
+}
